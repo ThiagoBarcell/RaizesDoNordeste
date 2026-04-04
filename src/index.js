323697f04dbf16ce31770f9f7ef7948a512572e6
ThiagoBarcell <thiago-barcellos@hotmail.com>
2026-04-03 21:22:57 -0300

    criado as rotas de exemplo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
-const app = express()
+const app = express()   
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('GET /')
 })
+
+app.post('/', (req, res) => {
+  res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+  res.send('PUT /')
+})
+
+app.delete('/', (req, res) => {
+  res.send('DELETE /')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
